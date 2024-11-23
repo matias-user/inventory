3580a95dfa56f8982a47a8468fce94e698607d74
Matías <matias.abarca2018@gmail.com>
2024-11-23 18:22:05 -0300

    Se agrega función de logout en navbar y path en url

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views.register import register
 from .views.login import login_user
+from django.contrib.auth.views import LogoutView
 
 app_name= 'account'
 urlpatterns = [
         path('register/', register, name='register'),
         path('login/', login_user, name='login'),
+        path('logout/', LogoutView.as_view(next_page='/home'), name='logout')
 ]
